e0987162ceca31afcf364c629774c5004c7b455c
aditya gore <adityagore.1993@hotmail.com>
2020-05-11 11:48:14 +0530

    Secomd Commit

--- src/main/java/technicalblog/controller/HomeController.java
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -1,40 +1,24 @@
 package technicalblog.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import technicalblog.model.Post;
-
+import technicalblog.service.PostService;
 import java.util.ArrayList;
-import java.util.Date;
 
 @Controller
 public class HomeController {
 
+
+    @Autowired
+    private PostService postService;
+
     @RequestMapping("/")
     public String getAllPosts(Model model){
 
-        Post post1 = new Post();
-        post1.setTitle("Post 1");
-        post1.setBody("Post 1 body");
-        post1.setDate(new Date());
-
-        Post post2 = new Post();
-        post2.setTitle("Post 2");
-        post2.setBody("Post 2 body");
-        post2.setDate(new Date());
-
-        Post post3 = new Post();
-        post3.setTitle("Post 3");
-        post3.setBody("Post 3 body");
-        post3.setDate(new Date());
-
-        ArrayList<Post> posts = new ArrayList<>();
-        posts.add(post1);
-        posts.add(post2);
-        posts.add(post3);
-
+        ArrayList<Post> posts = postService.getAllPosts();
         model.addAttribute("posts", posts);
 
         return "index";

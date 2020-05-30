7cee0abdc0a12e5dd923189c87ee2c58dbb509f0
aditya gore <adityagore.1993@hotmail.com>
2020-05-30 09:09:32 +0530

    CRUD Operations done

--- src/main/java/technicalblog/controller/PostController.java
+++ src/main/java/technicalblog/controller/PostController.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import technicalblog.model.Post;
 import technicalblog.service.PostService;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
@@ -18,9 +20,8 @@ public class PostController {
 
     @RequestMapping("/users/posts")
     public String getUserPosts(Model model){
-        ArrayList<Post> posts = postService.getOnePost();
+        List<Post> posts = postService.getAllPosts();
         model.addAttribute("posts", posts);
-
         return "users/posts";
     }
 
@@ -32,6 +33,25 @@ public class PostController {
     @RequestMapping(value = "/posts/create", method = RequestMethod.POST)
     public String createPost(Post newPost){
         postService.createPost(newPost);
+        return "redirect:/users/posts";
+    }
+    @RequestMapping(value = "/editPost", method = RequestMethod.GET)
+    public String editPost(@RequestParam(name = "postId") Integer postId, Model model){
+        Post post = postService.getPost(postId);
+        model.addAttribute("post", post);
+        return "posts/edit";
+    }
+
+    @RequestMapping(value = "/editPost", method = RequestMethod.PUT)
+    public String editPost(@RequestParam(name = "postId") Integer postId, Post updatedPost){
+        updatedPost.setId(postId);
+        postService.updatePost(updatedPost);
+        return "redirect:/users/posts";
+    }
+
+    @RequestMapping(value = "/deletePost", method = RequestMethod.DELETE)
+    public String deletePostSubmit(@RequestParam(name = "postId") Integer postId){
+        postService.deletePost(postId);
         return "redirect:/users/posts";
     }
 }
--- src/main/java/technicalblog/model/Post.java
+++ src/main/java/technicalblog/model/Post.java
@@ -1,15 +1,32 @@
 package technicalblog.model;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "posts")
 public class Post {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "title")
     private String title;
+    @Column(name = "body")
     private String body;
+    @Column(name = "date")
     private Date date;
 
     public String getTitle() {
         return title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public void setTitle(String title) {
